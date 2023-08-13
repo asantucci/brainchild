9f54e7cb5a3b4f75a2490a8e1982a14a9f3cf3f2
Andreas Santucci <santucci.andreas@gmail.com>
2023-08-12 21:25:51 -0700

    Adds documentation.

--- R/project_finance.R
+++ R/project_finance.R
@@ -14,6 +14,14 @@ ConvertRange <- function(range) {
 
 #' Downloads data for each ticker.
 #'
+#' Uses a try-catch to check if there's any sort of error when fetching the data
+#' and writing to desk.
+#'
+#' @param path_prefix A path prefix denoting where to write the data files
+#'     to disk.
+#' @param ticker A string describing a ticker.
+#' @param beg_date A date describing when to start fetching data from.
+#' @param end_date A date describing when to finish collecting data.
 #' @export
 GetDataForTicker <- function(path_prefix, ticker, beg_date, end_date) {
   # Try to download the data
@@ -32,6 +40,14 @@ GetDataForTicker <- function(path_prefix, ticker, beg_date, end_date) {
 }
 
 #' Cleans raw stock prices fetched from Yahoo (quantmod) API.
+#'
+#' The raw stock prices are fetched with column names that reflect the Ticker.
+#' We'd like to normalize the columns before joining together datasets from
+#' many different tickers. We perform minimal formatting on these datasets.
+#'
+#' @param stock_prices A raw file describing daily prices for a given ticker.
+#' @returns A data.frame with columns that are guaranteed to be consistent
+#'     across invocations of this helper function with different inputs.
 #' @export
 CleanStockPrices <- function(stock_prices) {
   setDT(stock_prices)
@@ -51,6 +67,29 @@ LENGTH_OF_LONG_TERM_TRADE_IN_DAYS = 180
 
 #' Calculates percent returns for short and long term trades.
 #'
+#' Takes as input a data.frame of stock prices (cleaned and well-formatted
+#' according to `CleanStockPrices()` above), as well as a data.frame
+#' describing Congressional Trades. Examines the expected yield of a short
+#' and long term trade.
+#'
+#' NOTE: This function currently doesn't consider the TRANSACTION column.
+#' I.e. whether the representative is buying or selling shares.
+#' This may be relevant in so far as: if a representative is selling their last
+#' set of shares, then they can't make any future profits.
+#'
+#' TODO: discuss with Thomas what the research question is, and learn how
+#' to more appropriately incorporate this "Transaction" column into our
+#' analysis.
+#'
+#' @param stock_prices A data.frame containing Tickers and Dates and Closing
+#'     prices for each day.
+#' @param congressional_trades A data.frame with 1-row per representative
+#'     x ticker x trade combination. Note that a representative can make
+#'     multiple transactions for the same ticker on the same day, in which
+#'     case that grouping of rows would repeat (with possibly different
+#'     Transaction types or amounts...).
+#' @returns A data.frame describing the expected profit per trade per
+#'     congressional individual.
 #' @export
 CalculateReturns <- function(stock_prices, congressional_trades) {
   setDT(stock_prices)
